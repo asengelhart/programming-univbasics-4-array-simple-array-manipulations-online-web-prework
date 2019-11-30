c0d19ffb6e2f98ad552709bec2b32a8206b5994e
Alex Engelhart <asengelhart@yahoo.com>
2019-11-30 20:25:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,3 +31,13 @@ def using_insert(array, elem)
 end
 
 def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, str)
+  array.delete(str)
+end
